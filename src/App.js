3f31fb0d46c9e4fd82c57da77eef9edbfc2b7f3c
karikolehmainen <kari.kolehmainen@vtt.fi>
2022-02-04 14:25:09 +0200

    fixed one bug on the App

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import Form from "@rjsf/core";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { datasheetSchemaObject } from './datasheet.js';
  
+var operation = 0;
+
 const handleSaveToPC = (jsonData,filename) => {
   const fileData = JSON.stringify(jsonData);
   const blob = new Blob([fileData], {type: "text/plain"});
   const url = URL.createObjectURL(blob);
   const link = document.createElement('a');
   console.log("download datasheet");
+  console.log(jsonData);
   link.download = `${filename}.json`;
   link.href = url;
   link.click();
@@ -20,19 +23,31 @@ const handleSaveToPC = (jsonData,filename) => {
 
 const sendToRAMP = (jsonData,filename) => {
     console.log("--- Send Datasheet to RAMP");
-
+ 
 }
 
 function onFormSubmit (event) {
     console.log("---Form submitted---");
     console.log(event.formData);
-    sendToRAMP(event.formData, "datasheet");
+    console.log(operation);
+    if(operation == 0)
+       sendToRAMP(event.formData, "datasheet");
+    else if (operation == 1)
+       handleSaveToPC(event.formData, "datasheet");
+}
+
+function submitToRAMP (event) {
+    console.log("---Submit To RAMP---");
+    operation = 0;
+}
+function loadFromFile (event) {
+    console.log("---Load From File---");
+    operation = 2;
 }
 
 function saveToFile (event) {
     console.log("---Save To file---");
-    console.log(event.formData);
-    handleSaveToPC(event.formData, "datasheet");
+    operation = 1;
 }
 
 function App() {
@@ -41,9 +56,11 @@ ReactDOM.render(
     <Form 
        schema={datasheetSchemaObject}
        onSubmit={onFormSubmit}
-    />
-    <button type="submit" className='btn btn-success'>Load from File</button>
+    >
+    <button type="submit" onClick={loadFromFile} className='btn btn-success'>Load from File</button>
+    <button type="submit" onClick={submitToRAMP} className='btn btn-success'>Submit to RAMP</button>
     <button type="submit" onClick={saveToFile} className="btn btn-success">Save to File</button>
+    </Form>
 </div>,
     document.getElementById("root")
 );
